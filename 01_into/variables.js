ce92de886f3c51e24c53084a453e1909ee9afa24
Akash <therealakash13@gmail.com>
2025-11-24 15:29:05 +0530

    function return type and parameter type

--- 01_into/variables.js
+++ 01_into/variables.js
@@ -5,3 +5,22 @@ console.log(greetings);
 var userId = 112233;
 // boolean
 var isLoggedIn = false;
+// any
+var hero;
+function getHero() {
+    return "Iron Man...";
+}
+hero = getHero();
+console.log("My favorite hero is ".concat(hero));
+// parameter type
+var addedNumber;
+function addTwo(num) {
+    return num + 2;
+}
+addedNumber = addTwo(5);
+// function return type
+function retString(name) {
+    return "Hello from ".concat(name);
+}
+var greet = retString("Akash");
+console.log(greet);
--- 01_into/variables.ts
+++ 01_into/variables.ts
@@ -14,7 +14,19 @@ let hero;
 function getHero() {
   return "Iron Man...";
 }
-
 hero = getHero();
-
 console.log(`My favorite hero is ${hero}`);
+
+// parameter type
+let addedNumber;
+function addTwo(num: number) {
+  return num + 2;
+}
+addedNumber = addTwo(5);
+
+// function return type
+function retString(name: string): string {
+  return `Hello from ${name}`;
+}
+const greet = retString("Akash");
+console.log(greet);
