f51c37c3870add1c18dfabf2dbd8ff3a348c6ed5
Akash <therealakash13@gmail.com>
2025-11-24 14:48:55 +0530

    number and boolean types

--- 01_into/variables.js
+++ 01_into/variables.js
@@ -1,2 +1,7 @@
+// string
 var greetings = "Hello from Akash";
 console.log(greetings);
+// number
+var userId = 112233;
+// boolean
+var isLoggedIn = false;
--- 01_into/variables.ts
+++ 01_into/variables.ts
@@ -1,3 +1,9 @@
-let greetings : string = "Hello from Akash";
-
+// string
+let greetings: string = "Hello from Akash";
 console.log(greetings);
+
+// number
+let userId = 112233;
+
+// boolean
+let isLoggedIn: boolean = false;
