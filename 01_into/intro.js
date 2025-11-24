50aba8872780e8cf727c13f199d6a2ee777a71c2
Akash <therealakash13@gmail.com>
2025-11-24 14:20:42 +0530

    intro to typescript and conversion to javascript

---
+++ 01_into/intro.js
@@ -0,0 +1,2 @@
+var user = { name: "Akash", age: 26 };
+console.log("I am ".concat(user.name, " of age ").concat(user.age));
---
+++ 01_into/intro.ts
@@ -0,0 +1,3 @@
+let user = { name: "Akash", age: 26 };
+
+console.log(`I am ${user.name} of age ${user.age}`);
