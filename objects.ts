eb7d7e6c080efe663f6740ac6e383655d5308dbb
Akash <therealakash13@gmail.com>
2025-11-24 17:39:50 +0530

    optional props

--- objects.ts
+++ objects.ts
@@ -7,3 +7,16 @@ const user = {
 function createCourse({ name, isPaid }: { name: string; isPaid: boolean }) {}
 
 createCourse({ name: "akash", isPaid: true });
+
+type User = {
+    name: string;
+    email:string;
+    isAdmin: boolean;
+    adminPass?: string;
+}
+
+const newUser:User={
+    name:"Akash",
+    email:"a@gmail.com",
+    isAdmin:false,
+}
