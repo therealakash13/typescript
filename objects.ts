203a84ca75d30a277abe1f8f02b03206598ddb56
Akash <therealakash13@gmail.com>
2025-11-24 16:58:41 +0530

    objects passing to functions

---
+++ objects.ts
@@ -0,0 +1,9 @@
+const user = {
+  name: "Akash",
+  email: "akash@gmail.com",
+  isAdmin: true,
+};
+
+function createCourse({ name, isPaid }: { name: string; isPaid: boolean }) {}
+
+createCourse({ name: "akash", isPaid: true });
